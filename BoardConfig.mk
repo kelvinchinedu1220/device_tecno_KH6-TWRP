604b6cf8d6434df85f53b28fdd8e715996c05091
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-02-16 11:16:21 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,9 @@ TW_INCLUDE_LPTOOLS := true
 # TWRP-Specific configuration
 TW_EXCLUDE_APEX := true
 
+# System 
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
 # Density / StatusBar
 TW_FRAMERATE := 60
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
