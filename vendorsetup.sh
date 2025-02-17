c6bba1889143c1b2a840b7eb1ea78fac6a5d2a36
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-02-17 14:48:05 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+#
+#	This file is part of the OrangeFox Recovery Project
 # 	Copyright (C) 2023 The OrangeFox Recovery Project
 #	
 #	OrangeFox is free software: you can redistribute it and/or modify
