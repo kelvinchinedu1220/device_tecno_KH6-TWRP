31eb34dea3039b866b457087b8600db8d3121cbb
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-02-18 20:34:03 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,28 +1,31 @@
 #
-# Copyright (C) 2023 The OrangeFox Recovery Project
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2023 The OrangeFox Recovery Project
+#	
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
 #
 
-FDEVICE="KH6"
-#set -o xtrace
+export FDEVICE="KH6"
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then 
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -32,41 +35,66 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+   	 export TW_DEFAULT_LANGUAGE="en"
+    	export LC_ALL="C"
+    	export FOX_AB_DEVICE=1
+    	export OF_USE_GREEN_LED=0
+    	export OF_FBE_METADATA_MOUNT_IGNORE=1
+    	export OF_USE_MAGISKBOOT=1
+    	export OF_USE_NEW_MAGISKBOOT=1
+      	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+       	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	    export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	    export OF_NO_MIUI_PATCH_WARNING=1
+	    export FOX_USE_BASH_SHELL=1
+	    export FOX_ASH_IS_BASH=1
+	    export FOX_USE_TAR_BINARY=1
+	    export FOX_USE_SED_BINARY=1
+	    export FOX_USE_XZ_UTILS=1
+	    export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	    export OF_QUICK_BACKUP_LIST="/boot;/data;"
+	    export OF_PATCH_AVB20=1
+	    export FOX_DELETE_AROMAFM=1
+	    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+	    export FOX_ENABLE_APP_MANAGER=0
+	    export FOX_DISABLE_APP_MANAGER=1
+        export FOX_DELETE_MAGISK_ADDON=1
 
-    export FOX_VERSION="R11.1"
-    export FOX_BUILD_TYPE=Unofficial
-    # export FOX_ENABLE_LAB=1
-    
-      export TARGET_DEVICE_ALT="KH6"
-      export OF_MAINTAINER="NINO"
+        # use system (ROM) fingerprint where available
+        export OF_USE_SYSTEM_FINGERPRINT=1
 
-    export OF_AB_DEVICE=1
-    export OF_USE_MAGISKBOOT=1
-    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	# OTA for custom ROMs
+	    export OF_KEEP_DM_VERITY=1
+        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
-    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+        # -- add settings for build version --
+        export FOX_VERSION="R11.1_2"
+        export OF_MAINTAINER="kelvin"
+        export FOX_BUILD_TYPE="UNOFFICIAL"
+        
+        # screen settings
+	    export OF_SCREEN_H=1612
+	    export OF_STATUS_H=40
+	    export OF_ALLOW_DISABLE_NAVBAR=0 # We don't have physical buttons
+	    export OF_STATUS_INDENT_LEFT=48
+	    export OF_STATUS_INDENT_RIGHT=48
+	    export OF_CLOCK_POS=1
+	    export OF_HIDE_NOTCH=0
 
-    export FOX_USE_NANO_EDITOR=1
-    export FOX_USE_BASH_SHELL=1
-    export FOX_USE_TAR_BINARY=1
-    export FOX_USE_XZ_UTILS=1
-    export TARGET_DEVICE_ALT="KH6"
+	# run a process after formatting data to work-around MTP issues
+    	export OF_RUN_POST_FORMAT_PROCESS=1
+	    export OF_SKIP_ORANGEFOX_PROCESS=1
+	
 
-    # Temp
-    export OF_NO_SPLASH_CHANGE=1
-
-    export FOX_REMOVE_AAPT=1
-    export FOX_DISABLE_APP_MANAGER=1
-    export FOX_DELETE_AROMAFM=1
-
-    export OF_STATUS_INDENT_LEFT=64
-    export OF_STATUS_INDENT_RIGHT=64
-
-    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-
-
-    # export FOX_BOARD_AVB_ENABLE=1
-    # export AVBTOOL=external/avb
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
 fi
 #
