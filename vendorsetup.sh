27ae57a6e688c6b71f84dbc22ae58a6e96c15891
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-02-18 21:40:54 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,44 +1,15 @@
 #
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2023 The OrangeFox Recovery Project
-#	
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# SPDX-License-Identifier: Apache-2.0
 #
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-# 	
-# 	Please maintain this if you use this script or any part of it
-#
+# reguire latest 
 
-export FDEVICE="KH6"
-
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
-
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
-
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	 export TW_DEFAULT_LANGUAGE="en"
+	export ALLOW_MISSING_DEPENDENCIES=true
+        export TW_DEFAULT_LANGUAGE="en"
     	export LC_ALL="C"
     	export FOX_AB_DEVICE=1
-    	export OF_USE_GREEN_LED=0
     	export OF_FBE_METADATA_MOUNT_IGNORE=1
     	export OF_USE_MAGISKBOOT=1
     	export OF_USE_NEW_MAGISKBOOT=1
