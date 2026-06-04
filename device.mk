72c4c56954f61128e8f016d5a8488c00c64a24b9
nino <pilopilosimon1990@gmail.com>
2026-06-04 12:12:10 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,14 +10,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/tecno/KH6
+
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Enable Virtual A/B OTA
